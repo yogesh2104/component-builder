1171a94e44757aa39e6de5358dfffd2ce7579171
Yogesh Singh <singhyogesh2104@gmail.com>
2025-03-05 01:10:13 +0530

    fix: change metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "AI-COmponent builder",
+  title: "AI-Component builder",
   description: "Created by Yogesh Singh",
 };
 
